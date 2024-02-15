0bfd27a6532593db3e76f86f9c9babd06012538a
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-02-15 09:55:33 +0200

    getInputFromUser function wes changed splitArguments function wes changed getLocatin function wes changed memmory lekage need to be fixed

--- myFunction.c
+++ myFunction.c
@@ -4,24 +4,64 @@
 void getLocation()
 {
     char location[SIZE_BUFF];
-    if (getcwd(location, SIZE_BUFF) == NULL)
-        printf("Error\n");
-    else
-    {
-        printf("\033[0;34m");
-        puts(location);
-        printf("\033[0m");
+//get the complete path of the current working directory including the name of the current user
+    struct passwd *pw;
+    pw = getpwuid(getuid());
+    if (pw == NULL) {
+        printf("Error getting user name\n");
+        return;
     }
+    if (getcwd(location, SIZE_BUFF) == NULL){
+        printf("Error getting current directory\n");
+        return;
+    }
+    printf("\033[0;34m");
+    printf("%s:%s$ ", pw->pw_name, location);
+    printf("\033[0m");
 }
 //  יש ליצור פונקציה הקולטת מחרוזת מהמשתמש  באופן דינמי כמו שעשינו בשיעור קודם
 char *getInputFromUser()
 {
-
-    return NULL;
+    char *input = (char *)malloc(SIZE_BUFF * sizeof(char));
+    if (input == NULL)
+    {
+        printf("Error\n");
+        return NULL;
+    }
+    printf("\033[0;32m");
+    fgets(input, SIZE_BUFF, stdin);
+    printf("\033[0m");
+    return input;
 }
 
 char **splitArgument(char *str)
 {
 
-    return NULL;
+    char **args = (char **)malloc(SIZE_BUFF * sizeof(char *));
+    if (args == NULL)
+    {
+        printf("Error\n");
+        return NULL;
+    }
+    int i = 0;
+    //So token is indeed pointing to the address of the first character in the first token of str.
+    char *token = strtok(str, " \t");
+    while (token != NULL)
+    {
+        args[i] = (char *)malloc((strlen(token) + 1) * sizeof(char));
+        if (args[i] == NULL)
+        {
+            printf("Error\n");
+            for (int j = 0; j < i; j++) {
+                free(args[j]);
+            }
+            free(args);
+            return NULL;
+        }
+        strcpy(args[i], token);
+        token = strtok(NULL, " \t");
+        i++;
+    }
+    args[i] = NULL;
+    return args;
 }
--- myShell.c
+++ myShell.c
@@ -3,11 +3,42 @@
 int main()
 {
     welcome();
+    char **args = NULL;
     while (1)
     {
         getLocation();
-        char* input = getInputFromUser();
+        char *input = getInputFromUser();
+        if (input == NULL)
+        {
+            printf("Error\n");
+            free(input);   
+            return 1;
+        }
+        if (strcmp(input, "exit\n") == 0)
+        {
+            free(input);
+            break;
+        }
+        args = splitArgument(input);
+        if (args == NULL)
+        {
+            printf("Error\n");
+            free(input);
+            return 1;
+        }
+        free(input);
     }
+    if (args != NULL)
+    {for(int i = 0; args[i] != NULL; i++)
+    {
+        printf("%s\n", args[i]);
+    }
+    printf("Goodbye\n");
+    printf("\033[0;31m");
+    for(int i = 0; args[i] != NULL; i++) {
+    free(args[i]);
+    }
+    free(args);}
     return 0;
 }
 
